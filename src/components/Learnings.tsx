9f58048c1ef0657c7630d58a79c3ba3248b54e09
William Phan <willdphan@gmail.com>
2023-03-27 17:00:32 -0400

    ui editz

--- src/components/Learnings.tsx
+++ src/components/Learnings.tsx
@@ -10,55 +10,121 @@ const Learnings = () => {
 		<section>
 			<div className="gap-6 columns-1 mt-32">
 				{/* LEARNINGS START */}
-				<div className="w-full bg-[#121212] rounded-lg mb-6">
-					<div className="py-10 px-10 space-y-5">
-						<h2 className="text-xl font-Space">Get. Plugged. In.</h2>
-						<h1 className="text-3xl font-Space font-medium">Development</h1>
-						<p className="text-xl leading-relaxed text-[#9B9B9B] font-Sans">
+				<div className="w-full bg-[#121212] transform transition duration-300 hover:scale-[1.10] hover:bg-gradient-to-br from-[#405580] via-[#8c9ab7] to-[#FFFFFF] text-[#9B9B9B] hover:text-white rounded-lg mb-6">
+					<div className="py-16 px-10 space-y-7">
+						<h2 className="text-xl font-Space text-white">Get. Plugged. In.</h2>
+						<h1 className="text-3xl font-Space font-medium text-white">Development</h1>
+						<p className="text-xl leading-relaxed  font-Sans">
 							Libraries for building performant and instant search experiences with Algolia. Compatible
 							with JavaScript, TypeScript, React and Vue.
 						</p>
 						<div className="flex space-x-10 text-xl text-[#405580]">
-							<Link href="mailto:willdphan@gmail.com">
-								<p>Notion</p>
-							</Link>
-							<Link href="mailto:willdphan@gmail.com">
-								<p>Contracts</p>
-							</Link>
+							{/* LINK 1 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Notion</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
+							{/* LINK 2 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Contracts</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
 						</div>
 					</div>
 				</div>
 				{/* LEARNINGS END */}
 				{/* LEARNINGS START */}
-				<div className="w-full bg-[#121212] rounded-lg mb-6">
-					<div className="py-10 px-10 space-y-5">
-						<h2 className="text-xl font-Space">Boosting My Creative Outlet</h2>
-						<h1 className="text-3xl font-Space font-medium">Design [UI/UX]</h1>
-						<p className="text-xl leading-relaxed text-[#9B9B9B] font-Sans">
+				<div className="w-full bg-[#121212] transform transition duration-300 hover:scale-[1.10] hover:bg-gradient-to-br from-[#405580] via-[#8c9ab7] to-[#FFFFFF] text-[#9B9B9B] hover:text-white rounded-lg mb-6">
+					<div className="py-16 px-10 space-y-7">
+						<h2 className="text-xl font-Space text-white">Get. Plugged. In.</h2>
+						<h1 className="text-3xl font-Space font-medium text-white">Development</h1>
+						<p className="text-xl leading-relaxed  font-Sans">
 							Libraries for building performant and instant search experiences with Algolia. Compatible
 							with JavaScript, TypeScript, React and Vue.
 						</p>
 						<div className="flex space-x-10 text-xl text-[#405580]">
-							<Link href="mailto:willdphan@gmail.com">
-								<p>Notion</p>
-							</Link>
+							{/* LINK 1 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Notion</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
+							{/* LINK 2 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Contracts</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
 						</div>
 					</div>
 				</div>
 				{/* LEARNINGS END */}
 				{/* LEARNINGS START */}
-				<div className="w-full bg-[#121212] rounded-lg mb-6">
-					<div className="py-10 px-10 space-y-5">
-						<h2 className="text-xl font-Space">Good Reads</h2>
-						<h1 className="text-3xl font-Space font-medium">My Library</h1>
-						<p className="text-xl leading-relaxed text-[#9B9B9B] font-Sans">
+				<div className="w-full bg-[#121212] transform transition duration-300 hover:scale-[1.10] hover:bg-gradient-to-br from-[#405580] via-[#8c9ab7] to-[#FFFFFF] text-[#9B9B9B] hover:text-white rounded-lg mb-6">
+					<div className="py-16 px-10 space-y-7">
+						<h2 className="text-xl font-Space text-white">Get. Plugged. In.</h2>
+						<h1 className="text-3xl font-Space font-medium text-white">Development</h1>
+						<p className="text-xl leading-relaxed  font-Sans">
 							Libraries for building performant and instant search experiences with Algolia. Compatible
 							with JavaScript, TypeScript, React and Vue.
 						</p>
 						<div className="flex space-x-10 text-xl text-[#405580]">
-							<Link href="mailto:willdphan@gmail.com">
-								<p>Notion</p>
-							</Link>
+							{/* LINK 1 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Notion</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
+							{/* LINK 2 */}
+							<div className="flex items-center hover:cursor-pointer">
+								<Link href="mailto:willdphan@gmail.com">
+									<p>Contracts</p>
+								</Link>
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28">
+									<path fill="none" d="M0 0h24v24H0z" />
+									<path
+										d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+										fill="#405580"
+									/>
+								</svg>
+							</div>
 						</div>
 					</div>
 				</div>
