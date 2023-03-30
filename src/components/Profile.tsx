390bccf96fdf776ca4fc6b5df869f27733debb30
William Phan <willdphan@gmail.com>
2023-03-29 21:28:53 -0400

    icons

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -31,8 +31,8 @@ const Profile = () => {
 						className="hidden sm:block"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
-						width="15"
-						height="15"
+						width="20"
+						height="20"
 					>
 						<path fill="none" d="M0 0h24v24H0z" />
 						<path
@@ -57,8 +57,8 @@ const Profile = () => {
 						className="hidden sm:block"
 						xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 24 24"
-						width="15"
-						height="15"
+						width="20"
+						height="20"
 					>
 						<path fill="none" d="M0 0h24v24H0z" />
 						<path
