69e227cfcbb6db39b3035401f295b3f9b0cb1a48
William Phan <willdphan@gmail.com>
2023-03-30 00:00:33 -0400

    added crafts

--- src/components/Crafts.tsx
+++ src/components/Crafts.tsx
@@ -3,60 +3,31 @@ import Image from 'next/image'
 import test from 'public/images/iphone-se-2020-2560x1440-dark-8k-22550.jpg'
 import test1 from 'public/images/wallpaper.jpg'
 import test2 from 'public/images/apple-macbook-pro-2021-2560x1440-abstract-colorful-apple-october-2021-23772.jpg'
+import opepen from 'public/images/ezgif.com-video-to-gif.gif'
+import nouns from 'public/images/ezgif.com-video-to-gif (1).gif'
+import marketplace from 'public/images/ezgif.com-video-to-gif (1) copy.gif'
+import dock from 'public/images/ezgif.com-video-to-gif (1) copy 2.gif'
+import chips from 'public/images/mac-chips.gif'
 
 const Crafts = () => {
 	return (
 		<section id="crafts">
 			<div className="gap-2 columns-2 mt-20 mb-20">
 				<div className="mb-2 mt-10">
-					<Image alt="image" className="w-full" src={test} />
+					<Image alt="image" className="w-full rounded-lg" src={opepen} />
 				</div>
 				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test1} />
+					<Image alt="image" className="w-full  rounded-lg" src={nouns} />
 				</div>
 				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test2} />
+					<Image alt="image" className="w-full  rounded-lg" src={marketplace} />
 				</div>
 				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test1} />
+					<Image alt="image" className="w-full  rounded-lg" src={dock} />
 				</div>
 				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test2} />
+					<Image alt="image" className="w-full  rounded-lg" src={chips} />
 				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full rounded-lg" src={test1} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test2} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full rounded-lg" src={test} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full rounded-lg" src={test2} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg " src={test} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full  rounded-lg" src={test1} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full rounded-lg" src={test2} />
-				</div>
-				<div className="mb-2">
-					<Image alt="image" className="w-full rounded-lg" src={test2} />
-				</div>
-
-				{/* <img className="w-full aspect-video mb-12" src="https://picsum.photos/500/300?random=1" />
-					<img className="w-full aspect-square mb-6" src="https://picsum.photos/500/300?random=2" />
-					<img className="w-full aspect-square mb-6" src="https://picsum.photos/500/300?random=3" />
-					<img className="w-full aspect-square mb-6" src="https://picsum.photos/500/300?random=4" />
-					<img className="w-full aspect-video mb-6" src="https://picsum.photos/500/300?random=5" />
-					<img className="w-full aspect-video mb-6" src="https://picsum.photos/500/300?random=6" />
-					<img className="w-full aspect-square mb-6" src="https://picsum.photos/500/300?random=7" />
-					<img className="w-full aspect-video mb-6" src="https://picsum.photos/500/300?random=8" />
-					<img className="w-full aspect-square mb-6" src="https://picsum.photos/500/300?random=9" /> */}
 			</div>
 		</section>
 	)
