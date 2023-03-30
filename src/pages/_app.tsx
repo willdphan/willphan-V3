2b0b8a4c7daefaf4a27f5d2db9d3637905f18321
William Phan <willdphan@gmail.com>
2023-03-30 00:22:51 -0400

    favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import 'tailwindcss/tailwind.css'
 import { ThemeProvider } from 'next-themes'
 import Web3Provider from '@/components/Web3Provider'
 import 'src/styles/globals.css'
+import Head from 'next/head'
+import favicon from 'public/images/favico.ico'
 
 const App = ({ Component, pageProps }) => {
 	return (
 		<ThemeProvider attribute="class">
 			<Web3Provider>
+				{/* FAVICON */}
+				<Head>
+					{/* LINK TITLE */}
+					<title>William Phan</title>
+					<link rel="shortcut icon" className="rounded-full" href="/images/favico.ico" />
+				</Head>
 				<Component {...pageProps} />
 			</Web3Provider>
 		</ThemeProvider>
