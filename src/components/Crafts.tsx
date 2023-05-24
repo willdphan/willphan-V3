07fe78e8728a14c088e54fc3fd695595256e4d38
William Phan <willdphan@gmail.com>
2023-05-23 21:40:33 -0400

    scale

--- src/components/Crafts.tsx
+++ src/components/Crafts.tsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
-import test from 'public/images/iphone-se-2020-2560x1440-dark-8k-22550.jpg'
-import test1 from 'public/images/wallpaper.jpg'
-import test2 from 'public/images/apple-macbook-pro-2021-2560x1440-abstract-colorful-apple-october-2021-23772.jpg'
-import opepen from 'public/images/ezgif.com-video-to-gif.gif'
-import nouns from 'public/images/ezgif.com-video-to-gif (1).gif'
-import marketplace from 'public/images/ezgif.com-video-to-gif (1) copy.gif'
-import dock from 'public/images/ezgif.com-video-to-gif (1) copy 2.gif'
+import opepen from 'public/images/opepen.gif'
+import nouns from 'public/images/nouns.gif'
+import marketplace from 'public/images/marketplace.gif'
+import dock from 'public/images/dock.gif'
 import chips from 'public/images/mac-chips.gif'
 import walletfeatures from 'public/images/wallet-features.gif'
-import opepenfolder from 'public/images/ezgif.com-optimize.gif'
+import opepenfolder from 'public/images/opepenfolder.gif'
 import playlist from 'public/images/playlist-2.gif'
 import grain from 'public/images/playlist-grain-2.gif'
 import cd from 'public/images/cd2.gif'
 import degen from 'public/images/degen.gif'
+import scale from 'public/images/scale.gif'
 
 const images = [
-	{ src: degen, alt: 'image' },
+	{ src: scale, alt: 'image', link: 'bw-scaleai.vercel.app' },
 	{ src: dock, alt: 'image' },
 	{ src: marketplace, alt: 'image' },
 	{ src: playlist, alt: 'image' },
+	{ src: degen, alt: 'image', link: 'https://degen-kappa.vercel.app/' },
 	{ src: opepen, alt: 'image' },
 	{ src: chips, alt: 'image' },
 	{ src: cd, alt: 'image' },
@@ -33,14 +32,16 @@ const Crafts = () => {
 	return (
 		<section id="crafts">
 			<div className="gap-2 columns-2 mt-20 mb-20">
-				{images.map((image, index) => (
+				{images.map((image, index, link) => (
 					<div key={index} className="mb-2">
-						<Image
-							alt={image.alt}
-							className="w-full rounded-lg border-[#121212] border-2"
-							priority={true}
-							src={image.src}
-						/>
+						<a href={image.link}>
+							<Image
+								alt={image.alt}
+								className="w-full rounded-lg border-[#121212] border-2"
+								priority={true}
+								src={image.src}
+							/>
+						</a>
 					</div>
 				))}
 			</div>
