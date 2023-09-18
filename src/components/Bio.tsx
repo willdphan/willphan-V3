2bce130a4e012ef26236daa8fc3e86c82794c749
William Phan <willdphan@gmail.com>
2023-09-18 14:53:00 -0400

    font, colors

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -10,12 +10,9 @@ const Bio = () => {
 	return (
 		<div className="pt-20 lg:fixed min-h-screen lg:pt-18 space-y-2 px-3 lg:px-10  ">
 			<h1 className="text-md font-Space font-medium text-white ">William Phan</h1>
-			{/* <h2 className="text-sm text-[#9B9B9B] font-Inter leading-relaxed font-normal">
-				Inherently Imperfectly Perfect
-			</h2> */}
 
 			<p className="text-sm text-[#9B9B9B] leading-relaxed font-Inter font-normal md:max-w-[50em] lg:max-w-[30em] xl:max-w-[35em]">
-				An <span className="underline underline-offset-2">Inherently Imperfectly Perfect</span> 21 year old
+				An <span className="text-[#d5d5d5] hover:underline">Inherently Imperfectly Perfect</span> 21 year old
 				developer dabbling in design. Interested in Computer Vision. Finding ways to make a difference through
 				technology while documenting my learnings. I have a weakness for diet coke & dogs. Avid grass toucher.
 			</p>
