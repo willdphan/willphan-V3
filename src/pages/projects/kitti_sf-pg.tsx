d0ebce5acd7cebe171e100922bcbba7343113fa0
William Phan <willdphan@gmail.com>
2023-10-14 15:51:28 -0400

    markdown

--- src/pages/projects/kitti_sf-pg.tsx
+++ src/pages/projects/kitti_sf-pg.tsx
@@ -18,14 +18,12 @@ calib = f.readlines()
 # get projection matrices (rectified left camera --> left camera (u,v,z))
 P_rect2_cam2 = np.array([float(x) for x in calib[25].strip().split(' ')[1:]]).reshape((3,4))
 
-
 # get rectified rotation matrices (left camera --> rectified left camera)
 R_ref0_rect2 = np.array([float(x) for x in calib[24].strip().split(' ')[1:]]).reshape((3, 3,))
 
 # add (0,0,0) translation and convert to homogeneous coordinates
 R_ref0_rect2 = np.insert(R_ref0_rect2, 3, values=[0,0,0], axis=0)
 R_ref0_rect2 = np.insert(R_ref0_rect2, 3, values=[0,0,0,1], axis=1)
-
 
 # get rigid transformation from Camera 0 (ref) to Camera 2
 R_2 = np.array([float(x) for x in calib[21].strip().split(' ')[1:]]).reshape((3,3))
@@ -192,7 +190,6 @@ def draw_scenario(canvas, imu_xyz, sf=12):
 		obj_y2 = obj_center[1] + 10
 
 		cv2.rectangle(canvas, (obj_x1, obj_y1), (obj_x2, obj_y2), (255, 0, 0), -1);
-
 
 	return canvas
 
