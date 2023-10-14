dc04d212a8ef048679061cce35b8365e78807953
William Phan <willdphan@gmail.com>
2023-10-14 16:03:39 -0400

    link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -92,156 +92,129 @@ const Fun = () => {
 			>
 				{filteredProjects.slice(0, showMore ? filteredProjects.length : 4).map(project => (
 					// px-7 py-7
-					/* changes */
-					// eslint-disable-next-line react/jsx-key
-					<Link
-						href={`/projects/${project.name.toLowerCase().replace(/ /g, '_')}-pg?projectName=${
-							project.name
-						}`}
-					>
-						<div
-							key={project.name}
-							className=" mb-2 my-masonry-grid_column w-full bg-[#0A0A0A]  border-[#262626] border-[1px] rounded-lg py-[1px]  px-[1px] space-y-2 transform transition duration-300 sm:hover:scale-[1.01] sm:hover:bg-gradient-to-br hover:bg-[#2a2929] ] text-[#9B9B9B] hover:text-white"
-						>
-							{project.video ? (
-								<div className="">
-									<video
-										autoPlay
-										loop
-										muted
-										playsInline
-										className="rounded-lg border-[#0A0A0A] border-[1px]"
-									>
-										<source src={project.video} type="video/mp4" />
-									</video>
-								</div>
-							) : (
-								project.image && (
-									<div className="">
-										{/* Specify the width */}
-										{/* Specify the height */}
-										{/* If you want to maintain aspect ratio */}
-										<Image
-											src={project.image}
-											alt={project.name}
-											width={600}
-											height={300}
-											layout="responsive"
-											className="rounded-lg border-[#0A0A0A] border-[1px]"
-										/>
-									</div>
-								)
-							)}
 
-							{/* changes */}
-							<h2 className="text-xs font-Space text-[#9B9B9B] tracking-widest px-3 pt-3">
-								{project.title}
-							</h2>
-							{/*  LINK ADDED HERE FOR PROJECTS PAGE */}
-							{/* <Link
+					<div
+						key={project.name}
+						className=" mb-2 my-masonry-grid_column w-full bg-[#0A0A0A]  border-[#262626] border-[1px] rounded-lg py-[1px]  px-[1px] space-y-2 transform transition duration-300 sm:hover:scale-[1.01] sm:hover:bg-gradient-to-br hover:bg-[#2a2929] ] text-[#9B9B9B] hover:text-white"
+					>
+						{project.video ? (
+							<div className="">
+								<video
+									autoPlay
+									loop
+									muted
+									playsInline
+									className="rounded-lg border-[#0A0A0A] border-[1px]"
+								>
+									<source src={project.video} type="video/mp4" />
+								</video>
+							</div>
+						) : (
+							project.image && (
+								<div className="">
+									{/* Specify the width */}
+									{/* Specify the height */}
+									{/* If you want to maintain aspect ratio */}
+									<Image
+										src={project.image}
+										alt={project.name}
+										width={600}
+										height={300}
+										layout="responsive"
+										className="rounded-lg border-[#0A0A0A] border-[1px]"
+									/>
+								</div>
+							)
+						)}
+						{/* changes */}
+						{/*  LINK ADDED HERE FOR PROJECTS PAGE */}
+						<Link
 							href={`/projects/${project.name.toLowerCase().replace(/ /g, '_')}-pg?projectName=${
 								project.name
 							}`}
-						> */}
+						>
+							<h2 className="text-xs font-Space text-[#9B9B9B] tracking-widest px-3 pt-3">
+								{project.title}
+							</h2>
+
 							<h1 className="text-md font-Space font-normal text-white px-3 pt-3">{project.name}</h1>
-							{/* </Link> */}
 
 							<p className="text-sm leading-7 font-Inter  text-[#9B9B9B] px-3">{project.description}</p>
+						</Link>
+						{/* changes */}
+						<div className="flex space-x-4 pt-1 px-3 pb-3 ">
 							{/* changes */}
-							<div className="flex space-x-4 pt-1 px-3 pb-3">
-								{/* changes */}
-								{project.demo && (
-									<a
-										href={project.demo}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
-									>
-										Demo
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="20"
-											height="20"
-										>
-											<path fill="none" d="M0 0h24v24H0z" />
-											<path
-												d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
-												fill="#54627a"
-											/>
-										</svg>
-									</a>
-								)}
-								{project.frontend && (
-									<a
-										href={project.frontend}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
-									>
-										Frontend
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="20"
-											height="20"
-										>
-											<path fill="none" d="M0 0h24v24H0z" />
-											<path
-												d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
-												fill="#54627a"
-											/>
-										</svg>
-									</a>
-								)}
-								{project.code && (
-									<a
-										href={project.code}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
-									>
-										Code
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="20"
-											height="20"
-										>
-											<path fill="none" d="M0 0h24v24H0z" />
-											<path
-												d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
-												fill="#54627a"
-											/>
-										</svg>
-									</a>
-								)}
-								{project.nbviewer && (
-									<a
-										href={project.nbviewer}
-										target="_blank"
-										rel="noopener noreferrer"
-										className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
-									>
-										NBViewer
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											width="20"
-											height="20"
-										>
-											<path fill="none" d="M0 0h24v24H0z" />
-											<path
-												d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
-												fill="#54627a"
-											/>
-										</svg>
-									</a>
-								)}
-								{/* Adding the Next.js Image component */}
-							</div>
+							{project.demo && (
+								<a
+									href={project.demo}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
+								>
+									Demo
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+										<path fill="none" d="M0 0h24v24H0z" />
+										<path
+											d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+											fill="#54627a"
+										/>
+									</svg>
+								</a>
+							)}
+							{project.frontend && (
+								<a
+									href={project.frontend}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
+								>
+									Frontend
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+										<path fill="none" d="M0 0h24v24H0z" />
+										<path
+											d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+											fill="#54627a"
+										/>
+									</svg>
+								</a>
+							)}
+							{project.code && (
+								<a
+									href={project.code}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
+								>
+									Code
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+										<path fill="none" d="M0 0h24v24H0z" />
+										<path
+											d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+											fill="#54627a"
+										/>
+									</svg>
+								</a>
+							)}
+							{project.nbviewer && (
+								<a
+									href={project.nbviewer}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="flex items-center hover:cursor-pointer font-Inter text-sm text-[#54627a] pr-3"
+								>
+									NBViewer
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+										<path fill="none" d="M0 0h24v24H0z" />
+										<path
+											d="M16.004 9.414l-8.607 8.607-1.414-1.414L14.589 8H7.004V6h11v11h-2V9.414z"
+											fill="#54627a"
+										/>
+									</svg>
+								</a>
+							)}
+							{/* Adding the Next.js Image component */}
 						</div>
-					</Link>
+					</div>
 				))}
 			</Masonry>
 			{filteredProjects.length > 3 && (
