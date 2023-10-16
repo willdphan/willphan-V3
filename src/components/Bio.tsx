17bbb5f97ff041af6e595cc7165e65090c4d505a
William Phan <willdphan@gmail.com>
2023-10-15 21:46:38 -0400

    edit

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
 
 const Bio = () => {
 	return (
-		<div className="pt-20 lg:fixed min-h-screen lg:pt-18 space-y-2 px-3 lg:px-10  ">
+		<div className="pt-20 lg:fixed min-h-screen bg-[#0A0A0A] lg:pt-18 space-y-2 px-3 lg:px-10  ">
 			<h1 className="text-md font-Space font-medium text-white ">William Phan</h1>
 
 			<p className="text-sm text-[#9B9B9B] leading-7 font-Inter font-normal md:max-w-[50em] lg:max-w-[30em] xl:max-w-[35em]">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,13 @@ import Bio from 'src/components/Bio'
 
 const Home: FC = () => {
 	return (
-		<div className="flex items-center justify-center bg-[#0A0A0A] ">
-			<div className="relative px-6 lg:px-10 xl:px-28 max-w-screen-2xl w-full	">
-				<div className="relative lg:w-6/12 inset-0">
+		<div className="flex items-center justify-center bg-[#0A0A0A] min-h-screen">
+			<div className="relative px-6 lg:px-10 xl:px-28 max-w-screen-2xl w-full ">
+				<div className="relative lg:w-6/12 inset-0 bg-[#0A0A0A] ">
 					<Bio />
 				</div>
 
-				<div className="w-full lg:w-6/12 ml-auto pt-14  ">
+				<div className="w-full lg:w-6/12 ml-auto pt-14 ">
 					<Projects />
 					{/* <Learnings /> */}
 					{/* <Crafts /> */}
