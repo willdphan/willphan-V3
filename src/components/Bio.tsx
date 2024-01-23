3c91e8aaaf7b153a6354ea40a1ca02dfc7bae85e
William Phan <willdphan@gmail.com>
2024-01-23 10:11:28 -0500

    proj desc

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -13,8 +13,8 @@ const Bio = () => {
 
 			<p className="text-sm text-[#9B9B9B] leading-7 font-Inter font-normal md:max-w-[50em] lg:max-w-[30em] xl:max-w-[35em]">
 				An <span className="text-[#d5d5d5] hover:underline">Inherently Imperfectly Perfect</span> 21 year old
-				developer dabbling in design. Interested in Computer Vision. Finding ways to make a difference through
-				technology while documenting my learnings. I have a weakness for diet coke & dogs. Avid grass toucher.
+				developer dabbling in design. Finding ways to make a difference through technology while documenting my
+				learnings. I have a weakness for diet coke & dogs. Avid grass toucher.
 			</p>
 			<div>
 				<Menu />
